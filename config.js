10eb69a7b0da8a3c686054eade734a9586ae6add
djdjdallas <109679919+msxDom@users.noreply.github.com>
2024-07-13 09:32:17 -0700

    commit

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ import themes from "daisyui/src/theming/themes";
 
 const config = {
   // REQUIRED
-  appName: "ShipFast",
+  appName: "Mail Blitz",
   // REQUIRED: a short description of your app for SEO tags (can be overwritten)
   appDescription:
-    "The NextJS boilerplate with all you need to build your SaaS, AI tool, or any other web app.",
+    "MailBlitz is a powerful email CRM designed for new SaaS owners and developers looking to enhance their outreach and boost their business. Effortlessly manage your email campaigns, personalize cold emails, track analytics, and keep your contacts organized—all from one intuitive platform",
   // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
-  domainName: "shipfa.st",
+  domainName: "MailBlitz.io",
   crisp: {
     // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (mailgun.supportEmail) otherwise customer support won't work.
     id: "",
